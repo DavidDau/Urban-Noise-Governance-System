d24195dbdbfffb3a7a62366536459b75fece05d2
David Dau <d.cyubahiro@alustudent.com>
2026-07-09 17:22:12 +0200

    Restore6

--- Web_Dev/backend/app/routes/analysis.py
+++ Web_Dev/backend/app/routes/analysis.py
@@ -1,13 +1,6 @@
-import os
-import tempfile
-import traceback
-
 from fastapi import APIRouter, UploadFile, File, Form
 
-from app.services.ml_service import predict_source
-
 router = APIRouter()
-
 
 @router.post("/predict")
 async def analyze_audio(
@@ -15,37 +8,6 @@ async def analyze_audio(
     venue_type: str = Form(...),
     recording_time: str = Form(...)
 ):
-
-    temp_path = None
-
-    try:
-        with tempfile.NamedTemporaryFile(
-            delete=False,
-            suffix=".wav"
-        ) as tmp:
-
-            tmp.write(await file.read())
-            temp_path = tmp.name
-
-        print("STEP 1 - File saved")
-
-        source, confidence = predict_source(temp_path)
-
-        print("STEP 2 - Prediction completed")
-
-        return {
-            "success": True,
-            "source": source,
-            "confidence": confidence
-        }
-
-    except Exception as e:
-        print("===================================")
-        print(type(e).__name__)
-        print(e)
-        print(traceback.format_exc())
-        raise
-
-    finally:
-        if temp_path and os.path.exists(temp_path):
-            os.remove(temp_path)
+    return {
+        "success": True
+    }
